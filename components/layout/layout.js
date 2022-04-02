07d1e7c55d4dac608bd3e705b11b4eddc8a54b54
Ekaterina Baliasnikova <Ekaterina.Baliasnikova@ecentria.com>
2022-04-02 11:29:03 +0300

    add calc rigth vh

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,8 +1,16 @@
 import styles from './layout.module.css'
-import Head from 'next/head'
+import { useEffect } from 'react';
 
 export default function Layout({
   children
 }) {
-  return <div className={styles.container}> {children} </div>
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  });
+  return (
+    <div className={styles.container}> {children} </div>
+  )
 }

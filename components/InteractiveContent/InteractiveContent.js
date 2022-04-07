4258444cbf29b4cd7a0ffe07a03ea0c0d466ae68
Ekaterina Baliasnikova <Ekaterina.Baliasnikova@ecentria.com>
2022-04-07 11:56:28 +0300

    update text

--- components/InteractiveContent/InteractiveContent.js
+++ components/InteractiveContent/InteractiveContent.js
@@ -4,7 +4,11 @@ import Marquee from '../marquee/marquee';
 export default function InteractiveContent () {
     return (
         <div className={styles.container}>
-            <p>I work at <Marquee index={1}>Intexsys</Marquee> these days.As a part of the team, I solving seemingly unsolvable problems.Over the past year, we have implemented several projects that have brought great benefits to the business.I&apos; m part of a complex e-commerce project and I&apos;m proud of it, but I haven&apos;t always worked for a big international company.</p>
-            <p>I used to work for the Russian publishing house <Marquee index={2}>Komitet</Marquee>. My task was to develop advertising integrations for media <a href="https://vc.ru/" target="_blank" rel="noreferrer">VC</a>, <a href="https://tjournal.ru/" target="_blank" rel="noreferrer">TJ</a> and <a href="https://dtf.ru/" target="_blank" rel="noreferrer">DTF</a>. The most difficult project was for URALSIB Bank, I did it alone in 2 weeks. And it was a huge success with the audience. People start a real battle trying to save businessmen from despair and broke.</p>
+            <p>I&apos;m currently working in <Marquee index={1}>«Intexsys»</Marquee>. As a part of an agile team, I solving seemingly unsolvable problems and constantly improve my skills.</p>
+            <p>This place is my first job in an international company. Before that, I worked on media. The most challenging company was <Marquee index={2}>Komitet</Marquee>.</p>
+            <p>My responsibilities were to develop and maintain marketing integrations for <a href="https://dtf.ru" rel="noreferrer" target="_blank">DTF</a>, <a href="https://vc.ru/" rel="noreferrer" target="_blank">VC</a>, and <a href="https://tjournal.ru/" rel="noreferrer" target="_blank">TJ</a>.</p>
+            <p>I&apos;m proud of every project which I have done. But mine most loved - <a href="https://vc.ru/special/uralsib-tamagochi" rel="noreferrer" target="_blank">Tamagotchi of Businessman</a>, for Uralsib Bank. This project caused a battle between users. Day and night they clicked on the love/hate buttons and leaded virtual Businessmen to success.</p>
+            <p>Unfortunately, special projects in this media don&apos;t live long. And many of them exist only on my GitHub now.</p>
+            <p>I started my frontend developer career at <a href="https://www.strelka.com/en" rel="noreferrer" target="_blank">«Strelka Institute for Media, Architecture and Design»</a></p>
         </div>
     )}

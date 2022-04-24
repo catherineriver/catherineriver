db0da8ec423c6b61ea9c4603f379679915431b40
Ekaterina Baliasnikova <Ekaterina.Baliasnikova@ecentria.com>
2022-04-24 13:10:03 +0300

    adjust font sizes

--- components/InteractiveContent/InteractiveContent.js
+++ components/InteractiveContent/InteractiveContent.js
@@ -5,29 +5,32 @@ import TooltipHandler from '../tooltipHandler/tooltipHandler';
 export default function InteractiveContent() {
   return (
     <div className={styles.container}>
-      <p>
-        I&apos;m currently working in<TooltipHandler index={0}>«Intexsys»</TooltipHandler>. As a part of an agile team, I solving seemingly unsolvable problems and constantly improve my skills.
-      </p>
-      <p>
-        This place is my first job in an international company. Before that, I worked on media. The most challenging company was
-        <TooltipHandler index={1}>Komitet</TooltipHandler>.
-      </p>
-      <p>
-        My responsibilities were to develop and maintain marketing integrations for
-        <a href="https://dtf.ru" rel="noreferrer" target="_blank">DTF</a>,
-        <a href="https://vc.ru/" rel="noreferrer" target="_blank">VC</a>, and
+      <div className={styles.block}>
+        I&apos;m currently working in&nbsp;<TooltipHandler index={0}>«Intexsys»</TooltipHandler>. As a part of an agile team, I solving seemingly unsolvable problems and constantly improve my skills.
+      </div>
+      <div className={styles.block}>
+        This place is my first job in an international company. Before that, I worked on media. The most challenging company was&nbsp;
+        <TooltipHandler index={1}>«Komitet»</TooltipHandler>.
+      </div>
+      <div className={styles.block}>
+        My responsibilities were to develop and maintain marketing integrations for&nbsp;
+        <a href="https://dtf.ru" rel="noreferrer" target="_blank">DTF</a>,&nbsp;
+        <a href="https://vc.ru/" rel="noreferrer" target="_blank">VC</a>, and &nbsp;
         <a href="https://tjournal.ru/" rel="noreferrer" target="_blank">TJ</a>.
-      </p>
-      <p>
+      </div>
+      <div className={styles.block}>
         I&apos;m proud of every project which I have done. But mine most loved -
         <a href="https://vc.ru/special/uralsib-tamagochi" rel="noreferrer" target="_blank">Tamagotchi of Businessman</a>, 
         for Uralsib Bank. This project caused a battle between users. Day and night they clicked on the love/hate buttons and leaded virtual Businessmen to success.
-      </p>
-      <p>Unfortunately, special projects in this media don&apos;t live long. And many of them exist only on my GitHub now.</p>
-      <p>
+      </div>
+      <div className={styles.block}>
+        Unfortunately, special projects in this media don&apos;t live long. 
+        And many of them exist only on my GitHub now.
+      </div>
+      <div className={styles.block}>
         I started my frontend developer career at
-        <a href="https://www.strelka.com/en" rel="noreferrer" target="_blank">«Strelka Institute for Media, Architecture and Design»</a>
-      </p>
+        <TooltipHandler index={2}>«Strelka Institute for Media, Architecture and Design»</TooltipHandler>.
+      </div>
     </div>
   );
 }

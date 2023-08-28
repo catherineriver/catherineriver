79469ab8e9a5c2de52ea65bfa2996ef2834c25fd
Ekaterina Baliasnikova <22520904+catherineriver@users.noreply.github.com>
2023-08-28 22:48:31 +0300

    add meta

--- pages/_document.js
+++ pages/_document.js
@@ -11,17 +11,16 @@ class HeadMeta extends Document {
                     <meta name="theme-color" content="#ffffff" />
                     <meta property="og:title" content="Ekaterina Baliasnikova | Front-end developer" />
                     <meta property="og:description" content="Let's work together!" />
-                    <meta property="og:image" content="https://harnault.dev/images/og-image.jpg" />
+                    <meta property="og:image" content="https://harnault.dev/og-image.jpg" />
                     <meta property="og:image:type" content="image/jpg" />
-                    <meta name="twitter:image" content="https://harnault.dev/images/og-image.jpg" />
+                    <meta name="twitter:image" content="https://harnault.dev/og-image.jpg" />
                     <meta name="twitter:image:type" content="image/jpg" />
                     <meta property="og:type" content="website" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                    <link rel="icon" type="image/png" href="/favicon.png" />
                     <link rel="manifest" href="/site.webmanifest" />
                     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a1a2cc" />
-                        <link rel="icon" href="/favicon.ico" />
                         <link
                             rel="preconnect"
                             href="https://fonts.gstatic.com"
